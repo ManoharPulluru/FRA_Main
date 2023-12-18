d41a288f66cbe07a602d73ad55d27fca1c8b41a4
amollgaikwad <amollaxman.g@gaiansolutions.com>
2023-12-18 14:49:11 +0530

    fixed css for uploadmediapopup

--- src/components/pages/communication/MediaUploadModal/Mediauploadmodal.jsx
+++ src/components/pages/communication/MediaUploadModal/Mediauploadmodal.jsx
@@ -1,40 +1,35 @@
-import React from 'react'
-import "./mediauploadmodal.css"
-import cloud from "./images/cloud.svg"
-import Fileuploading from '../Fileuploading/Fileuploading'
-const Mediauploadmodal = ({hideFileUpload}) => {
+import React from "react";
+import "./mediauploadmodal.css";
+import cloud from "./images/cloud.svg";
+import Fileuploading from "../Fileuploading/Fileuploading";
+const Mediauploadmodal = ({ hideFileUpload }) => {
   return (
-    <div className='Mediauploadcontainer'>
+    <div className="Mediauploadcontainer">
       <div className="modal">
         <div className="cloud-container">
-            <div className="innercontainer">
-                <img src={cloud} alt="upload_cloud" />
-                <text>Uploaded!</text>
-            </div>
+          <div className="innercontainer">
+            <img src={cloud} alt="upload_cloud" />
+            <text>Uploaded!</text>
+          </div>
         </div>
-        <div className="medias">
-          <div className="media-card">
-          <Fileuploading/>
-          </div>
-          <div className="media-card">
-          <Fileuploading/>
-          </div>
-          <div className="media-card last-card">
-          <Fileuploading/>
-          </div>
-          {/* <div className="media-card">
-          <Fileuploading/>
-          </div> */}
-      
+        <div className="media-card">
+          <Fileuploading />
         </div>
-        
+        <div className="media-card">
+          <Fileuploading />
+        </div>
+        <div className="media-card">
+          <Fileuploading />
+        </div>
         <div className="actions">
-            <button className="cancel" onClick={hideFileUpload}>Cancel</button>
-            <button className="share">Done</button>
+          <button className="cancel" onClick={hideFileUpload}>
+            Cancel
+          </button>
+          <button className="share">Done</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mediauploadmodal
+export default Mediauploadmodal;
