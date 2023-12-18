a161bcf74c44d68a1dd2d5415f84292d1d0104a1
VinatiRamadhenu <ramadhenuvinati@gmail.com>
2023-12-18 13:17:01 +0530

    I am committing first code

--- src/components/pages/maps/Maps.jsx
+++ src/components/pages/maps/Maps.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "./maps.css";
+// import SideBar from "../../sideBar/SideBar";
+// import Footer from "../../footer/Footer"
+import LawEnforcement from "./LawEnforcement/LawEnforcement";
 const Maps = () => {
   return (
-    <div>
-      maps
-    </div>
-  )
-}
+    <div className="mapsParent">
+      {/* <div className="sidebar"> */}
+        {/* <SideBar /> */}
+      {/* </div> */}
+      <div className="mapsBody">
+        <div className="bodyPart">
+          <LawEnforcement/>  
+        </div>
+        {/* <div className="footer"> */}
+        {/* <Footer /> */}
+      {/* </div> */}
+      </div>
 
-export default Maps
+    </div>
+  );
+};
+
+export default Maps;
