de1712e8ca8ddf72f14df2d25288d699c2ad183c
Tejal Wani <tejalwani2000@gmail.com>
2023-12-19 09:54:27 +0530

    added: Toggle_icons folder and events

--- src/components/toggle_icons/Toggle_icons.jsx
+++ src/components/toggle_icons/Toggle_icons.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import overlay_icon from "./images/overlay_icon.svg";
+import share_icon from "./images/share_icon.svg";
+import "./toggle_icons.css";
 
-
-const Toggle_icons = () => {
+const Toggle_icons = ({ toggleMapOverlay }) => {
   return (
-    <div className='mainApp'>  
-      
+    <div className='toggle_ico_container'>  
+      <div className="map_overlay_icon" onClick={toggleMapOverlay}>
+        <img src={overlay_icon} alt="overlay_icon" />
+      </div>
+      <div className="share-icon">
+        <img src={share_icon} alt="share_icon" />
+      </div>
+      <div className="txt-3d-toggle">
+        <div className="txt-3d">
+          <span>3D</span>
+        </div>
+        <div className="toggle-symbol">
+          
+        </div>
+      </div>
     </div>
   )
 }
